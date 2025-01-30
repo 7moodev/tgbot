a5be6a1ab2716c3ab91ede083cfe9ae69f3a4d5a
Your Name <you@exmple.com>
2025-01-30 16:01:34 +0700

    dev(utils): add os_utils

---
+++ backend/commands/utils/constants.py
@@ -0,0 +1,4 @@
+import os
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
+commands_output_path = os.path.join(root_path, 'backend/commands/outputs')
---
+++ backend/commands/utils/os_utils.py
@@ -0,0 +1,18 @@
+import os
+from utils.constants import commands_output_path
+
+debug_should_log = False
+
+def ensure_dir(dir_name: str) -> None:
+  output_dir = os.path.join(os.path.dirname(__file__), dir_name)
+  if (debug_should_log): print(output_dir)
+  os.makedirs(output_dir, exist_ok=True)
+
+
+def ensure_file(file_name: str, output_dir: str = commands_output_path) -> None:
+  if (debug_should_log): print(output_dir)
+  os.makedirs(output_dir, exist_ok=True)
+
+  output_file = os.path.join(output_dir, file_name)
+  if (debug_should_log): print(output_file)
+  return output_file
