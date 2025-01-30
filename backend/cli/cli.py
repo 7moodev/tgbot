34e063035bbf082ce35f629c8e909b7b5e766b96
Your Name <you@exmple.com>
2025-01-31 00:45:05 +0700

    feat(backend.cli): init cli file. Idea: make functions available through the CLI

--- backend/cli/cli.py
+++ backend/cli/cli.py
@@ -1,4 +1,14 @@
 import asyncio
+import sys
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from backend.commands.top_holders_holdings import get_top_holders_holdings
 from backend.commands.holding_distribution import get_holding_distribution
 from backend.commands.fresh_wallets import fresh_wallets
@@ -7,8 +17,11 @@ async def main():
     token = "9XS6ayT8aCaoH7tDmTgNyEXRLeVpgyHKtZk5xTXpump"
     print(token)
     limit = 50
+    BOT_USERNAME= os.environ.get('tgNAME')
+    print(BOT_USERNAME)
 
-    # # Fetch top holders' holdings
+
+    # Fetch top holders' holdings
     # top_holders = await get_top_holders_holdings(token, limit)
     # print("Top Holders' Holdings:", top_holders)
 
