9d40941f90601f1531f6becf09ef840d7fe1f6bf
Your Name <you@exmple.com>
2025-02-01 12:34:41 +0700

    dev(api): format

--- backend/commands/utils/api/birdeye_api_service.py
+++ backend/commands/utils/api/birdeye_api_service.py
@@ -6,10 +6,11 @@ import requests
 import time
 
 from .entities.api_entity import ApiResponse
+from .entities.historical_price_unix_entity import HistoricalPriceUnixEntity
 from .entities.token_overview_entity import TokenOverviewEntity
 from ..services.log_service import LogService
 
-birdeyeapi = os.environ.get('birdeyeapi')
+birdeyeapi = os.environ.get("birdeyeapi")
 CHAIN = "solana"
 BASE_URL = "https://public-api.birdeye.so"
 
@@ -18,12 +19,24 @@ REQUEST_SEMAPHORE = asyncio.Semaphore(SEMAPHORE_NUM)
 
 logger = LogService()
 
+BIRDEYE_API_ENDPOINTS = {
+    "historical_price_unix": "defi/historical_price_unix",
+    "history_price": "defi/history_price",
+    "token_overview": "defi/token_overview",
+    "token_holder": "defi/v3/token/holder",
+    "token_creation_info": "defi/token_creation_info",
+    "gainers_losers": "trader/gainers-losers",
+    "wallet_token_list": "v1/wallet/token_list",
+    "wallet_token_balance": "v1/wallet/token_balance",
+}
+
+
 class BirdeyeApiService:
     def __init__(self):
         self.headers = {
             "accept": "application/json",
             "chain": CHAIN,
-            "X-API-KEY": birdeyeapi
+            "X-API-KEY": birdeyeapi,
         }
 
     """
@@ -31,7 +44,10 @@ class BirdeyeApiService:
     =========================================== PRICE ===========================================
     ===========================================       ===========================================
     """
-    async def get_price(self, token:str=None, unix_time:int = None):
+
+    async def get_price(
+        self, token: str = None, unix_time: int = None
+    ) -> ApiResponse[HistoricalPriceUnixEntity]:
         """
         Get the price of a token at a given unix time, costs 5 credits per request
         """
@@ -47,11 +63,13 @@ class BirdeyeApiService:
             logger.log("Error getting price for", token, "at", unix_time, ":Birdeye")
             return None
         if response.status_code != 200:
-            if response.json()['success'] == False:
+            if response.json()["success"] == False:
                 return None
-        return response.json()['data']['value']
+        return response.json()["data"]["value"]
 
-    async def get_price_historical(self, token:str=None, type:str=None, start:int=None, end:int=None):
+    async def get_price_historical(
+        self, token: str = None, type: str = None, start: int = None, end: int = None
+    ):
         logger.log("Getting price historical for", token, "from", start, "to", end)
         """
         Get the price of a token at a given unix time, costs 5 credits per request
@@ -61,20 +79,28 @@ class BirdeyeApiService:
         try:
             response = requests.get(url, headers=self.headers)
         except:
-            logger.log("Error getting price historical for", token, "from", start, "to", end, ":Birdeye")
+            logger.log(
+                "Error getting price historical for",
+                token,
+                "from",
+                start,
+                "to",
+                end,
+                ":Birdeye",
+            )
             return None
         if response.status_code != 200:
-            if response.json()['success'] == False:
+            if response.json()["success"] == False:
                 return None
-        return response.json()['data']['items']
-
+        return response.json()["data"]["items"]
 
     """
     ===========================================        ===========================================
     =========================================== TRADER ===========================================
     ===========================================        ===========================================
     """
-    async def get_top_traders(self, type:str = '1W', limit:int = 10000):
+
+    async def get_top_traders(self, type: str = "1W", limit: int = 10000):
         """
         Costs 30 per call
         """
@@ -85,65 +111,75 @@ class BirdeyeApiService:
         #     array_of_json = [json.dumps(d) for d in array_of_objects]
         #     return array_of_json
         all_data = {}
-        if os.path.exists('top_traders.json'):
-            with open('top_traders.json', 'r') as file:
+        if os.path.exists("top_traders.json"):
+            with open("top_traders.json", "r") as file:
                 try:
                     all_data = json.load(file)
                 except json.JSONDecodeError:
-                    logger.log("Error loading 'top_traders.json'. File is empty or corrupt.")
+                    logger.log(
+                        "Error loading 'top_traders.json'. File is empty or corrupt."
+                    )
                     all_data = {}
-                for item in all_data.get('items',[]):
-                    if (int(time.time() - item.get('timestamp', 0)) < 7 * 24 * 60 * 60) and (item.get('type', '') == type):
+                for item in all_data.get("items", []):
+                    if (
+                        int(time.time() - item.get("timestamp", 0)) < 7 * 24 * 60 * 60
+                    ) and (item.get("type", "") == type):
                         logger.log("Returning cached data from 'top_traders.json'.")
-                        return item.get('items', [])
+                        return item.get("items", [])
         res = []
-        while(len(res) < limit):
+        while len(res) < limit:
 
             url = f"{BASE_URL}/trader/gainers-losers?type={type}&sort_by=PnL&sort_type=desc&offset={len(res)}&limit=10"
             response = requests.get(url, headers=self.headers)
-            if (response.status_code != 200):
+            if response.status_code != 200:
                 logger.log("Error getting top traders: ", response.json())
                 return res
-            res += response.json()['data']['items']
+            res += response.json()["data"]["items"]
         current_timestamp = str(int(time.time()))
         try:
-            if("items" not in all_data):
+            if "items" not in all_data:
                 all_data["items"] = []
-            all_data["items"].append({
-                "timestamp": int(current_timestamp),
-                "type": type,
-                "limit": limit,
-                "items": res,
-            })
+            all_data["items"].append(
+                {
+                    "timestamp": int(current_timestamp),
+                    "type": type,
+                    "limit": limit,
+                    "items": res,
+                }
+            )
         except NameError:
             logger.log("Creating new saved_data dictionary.")
         try:
-            with open('top_traders.json', 'w') as file:
+            with open("top_traders.json", "w") as file:
                 json.dump(all_data, file, indent=4)
-            logger.log(f"Saved top {len(res)} traders for {type} to 'top_traders.json'.")
+            logger.log(
+                f"Saved top {len(res)} traders for {type} to 'top_traders.json'."
+            )
         except Exception as e:
             logger.log(f"Error saving to 'top_traders.json': {e}")
         logger.log("Returning top {} traders for {}".format(limit, type))
         return res
 
-    async def get_wallet_trade_history(self, wallet:str, limit:int=100, before_time:int=0, after_time:int=0):
+    async def get_wallet_trade_history(
+        self, wallet: str, limit: int = 100, before_time: int = 0, after_time: int = 0
+    ):
 
-        if (limit == 0):
-            if os.path.exists('trade_history.json'):
-                with open('trade_history.json', 'r') as file:
+        if limit == 0:
+            if os.path.exists("trade_history.json"):
+                with open("trade_history.json", "r") as file:
                     data = json.load(file)
                     print("Returning cached data from 'trade_history.json'.")
                     return data
         print("Getting trade history for", wallet)
         res = []
-        while(len(res) < limit):
+        while len(res) < limit:
             url = f"{BASE_URL}/trader/txs/seek_by_time?address={wallet}&offset={len(res)}&limit=100&tx_type=swap&before_time={before_time}&after_time={after_time}"
             response = requests.get(url, headers=self.headers)
-            if (response.status_code != 200):
+            if response.status_code != 200:
                 print("Error getting trade_history: ", response.json())
                 return res
-            res += response.json()['data']['items']
-            if len(response.json()['data']['items']) < 100:
+            res += response.json()["data"]["items"]
+            if len(response.json()["data"]["items"]) < 100:
                 break
         # try:
         #     with open('trade_history.json', 'w') as file:
@@ -159,7 +195,10 @@ class BirdeyeApiService:
     =========================================== TOKEN ===========================================
     ===========================================       ===========================================
     """
-    async def get_token_overview(self, token:str=None) -> ApiResponse[TokenOverviewEntity]:
+
+    async def get_token_overview(
+        self, token: str = None
+    ) -> ApiResponse[TokenOverviewEntity]:
         """
         https://docs.birdeye.so/reference/get_defi-token-overview
         Get the overview of a token
@@ -174,7 +213,9 @@ class BirdeyeApiService:
         logger.log("Returning token overview for", token)
         return response.json()
 
-    async def get_top_holders_with_constraint(self, token:str=None, min_value_usd:float=None, price:float=None):
+    async def get_top_holders_with_constraint(
+        self, token: str = None, min_value_usd: float = None, price: float = None
+    ):
         """
         Get the top holders of a token that hold at least min_value_usd worth of tokens
 
@@ -200,18 +241,18 @@ class BirdeyeApiService:
             url = f"{BASE_URL}/defi/v3/token/holder?address={token}&offset={offset}&limit={batch_size}"
             response = requests.get(url, headers=self.headers)
             if response.status_code != 200:
-                if not response.json()['success']:
+                if not response.json()["success"]:
                     return None
 
-            batch = response.json()['data']['items']
+            batch = response.json()["data"]["items"]
 
             # Stop if we hit empty batch or zero amounts
-            if not batch or batch[0]['amount'] == '0' or batch[-1]['amount'] == '0':
+            if not batch or batch[0]["amount"] == "0" or batch[-1]["amount"] == "0":
                 break
 
             # Filter holders that meet minimum value
             for holder in batch:
-                value_usd = float(holder['uiAmount']) * price
+                value_usd = float(holder["uiAmount"]) * price
                 if value_usd >= min_value_usd:
                     all_holders.append(holder)
                 else:
@@ -222,7 +263,7 @@ class BirdeyeApiService:
 
         return all_holders
 
-    async def get_token_creation_info(self, token:str=None):
+    async def get_token_creation_info(self, token: str = None):
         """{
         "data": {
             "txHash": "4ePtuFmo3uYX5m2mqMYEVqUpJzCoRHymmrTnCk1q1KiyvhpmCABVQ1sq1CFLMWHZAwbicC8V1Ao664WumAqxWQ86",
@@ -240,20 +281,20 @@ class BirdeyeApiService:
 
         try:
             response = requests.get(url, headers=self.headers)
-            return response.json()['data']
+            return response.json()["data"]
         except Exception as e:
             logger.log("Error getting token creation info for", token, ":Birdeye")
             return None
 
-    async def get_top_holders(self, token:str=None, limit = None):
+    async def get_top_holders(self, token: str = None, limit=None):
         """
         Get the top holders of a token, costs 50 credits per request
         Iterates through all holders using offset pagination
         """
         logger.log("Getting top holders for", token, "with limit", limit)
-        if(limit == 0):
-            if os.path.exists('top_holders.json'):
-                with open('top_holders.json', 'r') as file:
+        if limit == 0:
+            if os.path.exists("top_holders.json"):
+                with open("top_holders.json", "r") as file:
                     logger.log("Returning cached data from 'top_holders.json'")
                     return json.load(file)
         all_holders = []
@@ -268,14 +309,18 @@ class BirdeyeApiService:
             url = f"{BASE_URL}/defi/v3/token/holder?address={token}&offset={offset}&limit={batch_size}"
             response = requests.get(url, headers=self.headers)
             if response.status_code != 200:
-                if response.json()['success'] == False:
+                if response.json()["success"] == False:
                     return None
-            batch = response.json()['data']['items']
-            if not batch or batch[0]['amount'] == '0' or batch[len(batch)-1]['amount'] == '0':
+            batch = response.json()["data"]["items"]
+            if (
+                not batch
+                or batch[0]["amount"] == "0"
+                or batch[len(batch) - 1]["amount"] == "0"
+            ):
                 if len(all_holders) == 0:
                     all_holders.extend(batch)
                 break
-            #logger.log(len(batch))
+            # logger.log(len(batch))
             all_holders.extend(batch)
             # If we have a limit and reached/exceeded it, trim and break
             if limit is not None and len(all_holders) >= limit:
@@ -290,24 +335,28 @@ class BirdeyeApiService:
             else:
                 break
         for holder in all_holders:
-            res.append(holder['owner'])
+            res.append(holder["owner"])
         try:
-            with open('top_holders.json', 'w') as file:
+            with open("top_holders.json", "w") as file:
                 json.dump(all_holders, file, indent=4)
-            logger.log(f"Saved top {len(res)} holders for {type} to 'top_holders.json'.")
+            logger.log(
+                f"Saved top {len(res)} holders for {type} to 'top_holders.json'."
+            )
         except Exception as e:
             logger.log(f"Error saving to 'top_holders.json': {e}")
         logger.log("Returning top holders for", token)
         logger.log("Returned", len(all_holders), "holders")
         return all_holders
 
-
     """
     ===========================================        ===========================================
     =========================================== WALLET ===========================================
     ===========================================        ===========================================
     """
-    async def get_wallet_token_list(self, address: str, session: httpx.AsyncClient = None):
+
+    async def get_wallet_token_list(
+        self, address: str, session: httpx.AsyncClient = None
+    ):
         """
         https://docs.birdeye.so/reference/get_defi-tokenlist
 
@@ -326,35 +375,45 @@ class BirdeyeApiService:
                 if response.status_code != 200:
                     return {"error": f"Failed to fetch portfolio for wallet {address}"}
 
-                data = response.json()['data']
+                data = response.json()["data"]
 
-                if not data.get('success', True):
+                if not data.get("success", True):
                     return {"error": f"Failed to fetch portfolio for wallet {address}"}
                 logger.log(f"Fetched portfolio for wallet {address}")
                 return data
             except Exception as e:
-                return {"error": f"Exception fetching portfolio for wallet {address}: {str(e)}"}
+                return {
+                    "error": f"Exception fetching portfolio for wallet {address}: {str(e)}"
+                }
 
     async def get_balance_birdeye(self, wallet, token):
         print("Getting balance using Birdeye for", wallet, "in", token)
 
-        url = f"{BASE_URL}/v1/wallet/token_balance?wallet={wallet}&token_address={token}"
+        url = (
+            f"{BASE_URL}/v1/wallet/token_balance?wallet={wallet}&token_address={token}"
+        )
 
         try:
             response = requests.get(url, headers=self.headers)
             if response.status_code != 200:
                 return None
-            data = response.json()['data']
+            data = response.json()["data"]
             print(f"Fetched balance for wallet {wallet} in {token}")
             return data
         except Exception as e:
-            print(f"Error fetching balance using Birdeye for wallet {wallet} in {token}: {str(e)}")
+            print(
+                f"Error fetching balance using Birdeye for wallet {wallet} in {token}: {str(e)}"
+            )
             return None
 
 
 if __name__ == "__main__":
-  async def call():
-    service = BirdeyeApiService()
-    response = await service.get_token_overview("0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9")
-    response.data
-  call()
+
+    async def call():
+        service = BirdeyeApiService()
+        response = await service.get_token_overview(
+            "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
+        )
+        response.data
+
+    call()
