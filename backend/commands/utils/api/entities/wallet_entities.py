a37910e04316a44156f61908d71fa28548ec9ab2
Your Name <you@exmple.com>
2025-02-03 17:54:38 +0700

    dev(db): add wallet_token_balance db

--- backend/commands/utils/api/entities/wallet_entities.py
+++ backend/commands/utils/api/entities/wallet_entities.py
@@ -69,3 +69,16 @@ class WalletTokenBalanceEntity(WalletEntity, WalletBalance):
     """
 
     pass
+
+
+Mock_WalletTokenBalanceEntity = {
+    "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    "name": "Tether USD",
+    "symbol": "USDT",
+    "decimals": 6,
+    "balance": 94149341656019,
+    "uiAmount": 94149341.656019,
+    "chainId": "eth-mainnet",
+    "priceUsd": 0.9999915198446391,
+    "valueUsd": 94149341656019.2,
+}
