82fb31bf4701eabf332779c36ec97f16ed5577e6
Your Name <you@exmple.com>
2025-02-16 10:05:35 +0700

    dev(x.api): rename env variables

--- backend/commands/utils/api/x_api_service.py
+++ backend/commands/utils/api/x_api_service.py
@@ -15,10 +15,10 @@ load_dotenv()
 BASE_URL = "https://api.x.com/2/tweets"
 BEARER_TOKEN = os.environ.get("X_BEARER_TOKEN")
 client = tweepy.Client(
-    consumer_key=os.environ.get("API_KEY"),
-    consumer_secret=os.environ.get("API_KEY_SECRET"),
-    access_token=os.environ.get("ACCESS_TOKEN"),
-    access_token_secret=os.environ.get("ACCESS_TOKEN_SECRET"),
+    consumer_key=os.environ.get("X_API_KEY"),
+    consumer_secret=os.environ.get("X_API_KEY_SECRET"),
+    access_token=os.environ.get("X_ACCESS_TOKEN"),
+    access_token_secret=os.environ.get("X_ACCESS_TOKEN_SECRET"),
 )
 
 
